374f9d0a281e8e5df6f6976936b7efc13a788aa9
Mamoun Hisham <mamounprogrammer@gmail.com>
2025-05-17 05:52:29 +0300

    deleting uneccessery route

--- routes/web.php
+++ routes/web.php
@@ -23,11 +23,9 @@ Route::view('/contact', 'contact')->name('contact');
 Route::get('/clients/{client}', Home::class)->name('client.home')->middleware(AuthenticationCheck::class);
 Route::get('/clients/{client}/login', Login::class)->name('client.login');
 Route::get('/clients/{client}/register', Register::class)->name('client.register');
-// Route::get('/clients/{client}/lessons', Lesson::class)->name('client.lessons');
 Route::get('/clients/{client}/assignments', Assignment::class)->name('client.assignments');
 Route::get('/clients/{client}/lectures', Lecture::class)->name('client.lectures');
 Route::get('/clients/{client}/subjects', Subject::class)->name('client.subjects');
-Route::get('/clients/{client}/subjects/{subject}/lessons', Lesson::class)->name('client.lessons');
 Route::get('/clients/{client}/subjects/{subject}/lessons/{lesson}', Lesson::class)->name('client.lessons.show');
 
 Route::middleware(['auth'])->group(function () {
